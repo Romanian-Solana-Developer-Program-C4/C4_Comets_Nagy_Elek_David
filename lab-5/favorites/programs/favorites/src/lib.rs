134ca9840ae008c750619a3407cd45a1994461de
TTC <theomaniac917@gmail.com>
2025-09-16 14:32:24 +0300

    lab 5 - almost there :)

--- lab-5/favorites/programs/favorites/src/lib.rs
+++ lab-5/favorites/programs/favorites/src/lib.rs
@@ -1,3 +1,6 @@
+#![allow(unexpected_cfgs)]
+#![allow(deprecated)] // optional, silences internal realloc warning
+
 use anchor_lang::prelude::*;
 
 declare_id!("FyNhvMsRVReL6akKr4mjKZRV2hmxk7JTKEsXZdjoSfeG");
@@ -6,28 +9,31 @@ declare_id!("FyNhvMsRVReL6akKr4mjKZRV2hmxk7JTKEsXZdjoSfeG");
 pub mod favorites {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
-    }
-
     pub fn set_favorite(ctx: Context<SetFavorite>) -> Result<()> {
         let favorite = &mut ctx.accounts.favorites;
+
+        // On-chain logic: store numbers and color
         favorite.number = 1;
         favorite.color = 2;
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
-
-#[derive(Accounts)]
 pub struct SetFavorite<'info> {
     #[account(mut)]
     pub user: Signer<'info>,
-    #[account(init, payer = user, space = 8 + Favorites::INIT_SPACE)]
+
+    #[account(
+        init,
+        payer = user,
+        space = 8 + Favorites::INIT_SPACE,
+        seeds = [b"favorites".as_ref(), user.key().as_ref()],
+        bump
+    )]
     pub favorites: Account<'info, Favorites>,
+
     pub system_program: Program<'info, System>,
 }
 
@@ -36,6 +42,7 @@ pub struct SetFavorite<'info> {
 pub struct Favorites {
     pub number: u64,
     pub color: u8,
-    #[max_len(5,50)]
-    pub hobbies: Vec<String>,
+    #[max_len(5, 50)]
+    pub hobbies: Vec<String>, // stored in account heap, BPF-safe
+    pub user: Pubkey,
 }
