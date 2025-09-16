6bfd658e50847c2c53c27976b4ddf1f3f7cb895d
TTC <theomaniac917@gmail.com>
2025-09-16 11:34:16 +0300

    update lab 5

--- lab-5/favorites/programs/favorites/src/lib.rs
+++ lab-5/favorites/programs/favorites/src/lib.rs
@@ -10,7 +10,32 @@ pub mod favorites {
         msg!("Greetings from: {:?}", ctx.program_id);
         Ok(())
     }
+
+    pub fn set_favorite(ctx: Context<SetFavorite>) -> Result<()> {
+        let favorite = &mut ctx.accounts.favorites;
+        favorite.number = 1;
+        favorite.color = 2;
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+#[derive(Accounts)]
+pub struct SetFavorite<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(init, payer = user, space = 8 + Favorites::INIT_SPACE)]
+    pub favorites: Account<'info, Favorites>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Favorites {
+    pub number: u64,
+    pub color: u8,
+    #[max_len(5,50)]
+    pub hobbies: Vec<String>,
+}
