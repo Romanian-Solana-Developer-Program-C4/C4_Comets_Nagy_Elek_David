8f4d4390cc06eb91f5c59650cc045d2982c56dfa
TTC <theomaniac917@gmail.com>
2025-09-19 11:21:03 +0300

    lab 9 - intermediary

---
+++ lab-9/nft-staking/programs/nft-staking/src/context/initialize_config.rs
@@ -0,0 +1,51 @@
+use crate::state::StakeConfig;
+use anchor_lang::prelude::*;
+use anchor_spl::token::{Mint, Token};
+
+#[derive(Accounts)]
+#[instruction(max_stake: u8, freeze_period: i32, points_per_stake: u8)]
+pub struct InitializeConfig<'info> {
+    #[account(mut)]
+    pub authority: Signer<'info>,
+
+    #[account(
+        init,
+        payer = authority,
+        seeds = [b"config".as_ref(), authority.key().as_ref()],
+        space = 8 + StakeConfig::INIT_SPACE,
+        bump,
+    )]
+    pub stake_config: Account<'info, StakeConfig>,
+
+    #[account(
+        init,
+        payer = authority,
+        seeds = [b"rewards".as_ref(), stake_config.key().as_ref()],
+        bump,
+        mint::decimals = 6,
+        mint::authority = stake_config,
+    )]
+    pub rewards_mint: Account<'info, Mint>,
+
+    pub token_program: Program<'info, Token>,
+    pub system_program: Program<'info, System>,
+}
+
+impl<'info> InitializeConfig<'info> {
+    pub fn initialize_config(
+        &mut self,
+        max_stake: u8,
+        freeze_period: i32,
+        points_per_stake: u8,
+        bumps: &InitializeConfigBumps,
+    ) -> Result<()> {
+        self.stake_config.set_inner(StakeConfig {
+            max_stake,
+            freeze_period,
+            points_per_stake,
+            bump: bumps.stake_config,
+            rewards_bump: bumps.rewards_mint,
+        });
+        Ok(())
+    }
+}
--- lab-9/nft-staking/programs/nft-staking/src/lib.rs
+++ lab-9/nft-staking/programs/nft-staking/src/lib.rs
@@ -4,15 +4,27 @@ use anchor_lang::prelude::*;
 
 declare_id!("AESehE9tH7VxNKfcBspX7ZkE3F5zupjHNFMhn9edrw5t");
 
+pub mod context;
+pub mod state;
+
+pub use context::*;
+// pub use state::*;
+
 #[program]
 pub mod nft_staking {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize(
+        ctx: Context<InitializeConfig>,
+        max_stake: u8,
+        freeze_period: i32,
+        points_per_stake: u8,
+    ) -> Result<()> {
+        msg!("Initialize staking program: {:?}", ctx.program_id);
+
+        let _ =
+            ctx.accounts
+                .initialize_config(max_stake, freeze_period, points_per_stake, &ctx.bumps);
         Ok(())
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
