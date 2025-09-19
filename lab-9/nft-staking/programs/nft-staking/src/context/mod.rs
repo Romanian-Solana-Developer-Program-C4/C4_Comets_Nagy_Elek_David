3b69b1565ad8af2e017843657221be42a4f76910
TTC <theomaniac917@gmail.com>
2025-09-19 10:14:56 +0300

    lab 9 - reinit

--- lab-9/nft-staking/programs/nft-staking/src/context/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod initialize_config;
-pub mod initialize_user;
-pub mod stake;
--- lab-9/nft-staking/programs/nft-staking/src/state/stake_account.rs
+++
@@ -1,8 +0,0 @@
-use anchor_lang::prelude::*;
-
-#[account]
-#[derive(InitSpace)]
-pub struct StakeAccount {
-    owner: Pubkey,
-    mint
-}
